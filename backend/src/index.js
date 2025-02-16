bc0df9d66523aa4909012de0f1027a2d5f1129f3
amang9446 <dev.aman7571@gmail.com>
2025-02-16 19:45:53 +0530

    chore: configure CORS to allow all origins for now

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,7 +9,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "*", // Allowed all origins for now
+  })
+);
 app.use(express.json());
 
 // Routes
